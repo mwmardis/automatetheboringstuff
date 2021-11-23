11d38aec93d360548b51fa3b847e25b21876ecf5
Michael Mardis <MM061074@abcsupply.com>
2021-11-23 15:38:48 -0600

    while loop

---
+++ while.py
@@ -0,0 +1,11 @@
+spam = 0
+while spam < 5:
+    print('Spam')
+    spam = spam + 1
+
+
+name = ''
+while name != 'Your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
