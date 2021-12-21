2d428bdc97001329a84ee3c70af2e57b0c82a0cd
Michael Mardis <MM061074@abcsupply.com>
2021-12-21 11:01:22 -0600

    dictionaries are fun

---
+++ characterCount.py
@@ -0,0 +1,7 @@
+message = 'it was a bring cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] +1
+
+print(count)
---
+++ dictionary.py
@@ -0,0 +1,27 @@
+myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+print(myCat['size'])
+
+spam = {12345: 'luggage combo', 42: 'The answer'}
+print(spam)
+
+print('size' in myCat)
+print(list(myCat.keys()))
+
+for k in myCat.values():
+    print(k)
+    
+    
+for k, v in myCat.items():
+    print(k, v)
+    
+print('fat' in myCat.values())
+
+print(myCat.get('size', 0))
+
+print(myCat.get('asdf', 0))
+
+print('I am bringing ' + str(myCat.get('asdf', 0)) + ' cats to the picnic')
+
+myCat.setdefault('color', 'black')
+
+print(myCat)
