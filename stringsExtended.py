329d19fcea8641b5cec298b9b6e949944749029f
Michael Mardis <MM061074@abcsupply.com>
2021-12-21 11:20:27 -0600

    string syntax

---
+++ stringsExtended.py
@@ -0,0 +1,17 @@
+bobsMom = 'say hi to bob\'s mom'
+
+print(bobsMom)
+
+print('Hell there!\nHow are you?\nI\'m fine')
+
+print(r'That is carol\'s cat')
+
+print("""Dear Alice,
+      Eve's cat has been arrested for catnapping, cat burglary, and extortion. 
+      Sincerely,
+      Bob.""")
+
+print(bobsMom[1:5])
+print(bobsMom[-1])
+
+
